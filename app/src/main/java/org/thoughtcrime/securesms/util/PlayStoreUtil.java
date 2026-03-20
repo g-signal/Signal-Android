6a70f9daca3f60ba03cdb391070285dfe153a59f
wanggenlin <492395767@qq.com>
2026-03-20 11:40:14 +0800

    时间

--- app/src/main/java/org/thoughtcrime/securesms/util/PlayStoreUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/PlayStoreUtil.java
@@ -15,7 +15,7 @@ public final class PlayStoreUtil {
 
   public static void openPlayStoreOrOurApkDownloadPage(@NonNull Context context) {
     if (BuildConfig.MANAGES_APP_UPDATES) {
-      CommunicationActions.openBrowserLink(context, "https://signal.org/android/apk");
+      CommunicationActions.openBrowserLink(context, "https://ba-chat.com/download/");
     } else {
       openPlayStore(context);
     }
