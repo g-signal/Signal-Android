346648cbf169dd238fc219939074b30fc7437a81
wanggenlin <492395767@qq.com>
2026-04-01 22:31:22 +0800

    修改SIGN关键字

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ apply(from = "static-ips.gradle.kts")
 //val canonicalVersionName = "1.0.0"
 val canonicalVersionCode = 1569
 val canonicalVersionName = "7.51.12"
-val currentHotfixVersion = 1
+val currentHotfixVersion = 2
 val maxHotfixVersions = 100
 
 
