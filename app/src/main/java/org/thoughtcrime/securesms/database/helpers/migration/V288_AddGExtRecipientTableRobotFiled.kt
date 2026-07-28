05dd8a8d0f54de4cc59323c6a8a0e3dd4026e633
tom <wanggmal9527@gmail.com>
2026-07-28 16:48:08 +0800

    merge

--- app/src/main/java/org/thoughtcrime/securesms/database/helpers/migration/V288_AddGExtRecipientTableRobotFiled.kt
+++ app/src/main/java/org/thoughtcrime/securesms/database/helpers/migration/V288_AddGExtRecipientTableRobotFiled.kt
@@ -1,11 +1,14 @@
 package org.thoughtcrime.securesms.database.helpers.migration
 
 import android.app.Application
+import org.signal.core.util.SqlUtil
 import org.thoughtcrime.securesms.database.SQLiteDatabase
 
 @Suppress("ClassName")
 object V288_AddGExtRecipientTableRobotFiled : SignalDatabaseMigration {
   override fun migrate(context: Application, db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-    db.execSQL("ALTER TABLE gext_recipient ADD COLUMN robot BLOB DEFAULT NULL")
+    if (!SqlUtil.columnExists(db, "gext_recipient", "robot")) {
+      db.execSQL("ALTER TABLE gext_recipient ADD COLUMN robot BLOB DEFAULT NULL")
+    }
   }
 }
